233d1addb5a76c306532c4bc4667dd27a7968365
Josh Oynick <oynickj@gmail.com>
2013-12-18 14:56:21 -0500

    formatting adjustments

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,7 @@ class LocationsController < ApplicationController
 
   def index
     sushi = Foursquare.find_sushi("New York, NY")
+    bars = Foursquare.find_bars("New York, NY")
     render :json => sushi.to_json
   end
 
@@ -9,6 +10,11 @@ class LocationsController < ApplicationController
   def find_sushi_city
     sushi = Foursquare.find_sushi(params["city"])
     render :json => sushi.to_json
+  end
+
+  def find_bars_city
+    bars = Foursquare.find_bars(params["city"])
+    render :json => bars.to_json
   end
 
 
