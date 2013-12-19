b30f61d862415f5cee23976894ea322023124e95
Josh Oynick <oynickj@gmail.com>
2013-12-19 11:03:58 -0500

    added categories and fixed ticks

--- checkins_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CheckinsApp::Application.config.secret_token = '85b0d584e3ba57770961f5bbd20e8a4ddb9b2ba831e481c2fafd965a7c44df19ed2d3a0a7065b53b0449696b3736754f742c9a347afab49bceddabc60cd629c9'
